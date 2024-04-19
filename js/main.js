5eae88b34a972fbf9bb4490ceac1f50f19ca085a
OugMontiel <88790286+OugMontiel@users.noreply.github.com>
2024-04-19 15:12:21 -0500

    feat: :sparkles: Ejercisio 7 Finalizado

---
+++ js/main.js
@@ -0,0 +1,6 @@
+import prompt from 'async-prompt'
+import contarRango from './modulos/ej7.js'
+
+console.log(contarRango(1, 9)) // 7
+console.log(contarRango(1332, 8743)) // 7410
+console.log(contarRango(5, 6)) // 0
---
+++ js/modulos/ej7.js
@@ -0,0 +1,9 @@
+let contarRango = (num1,num2) =>{
+    let contador =0;
+    for (let i=num1+1;i<num2;i++){
+        contador++
+    }
+    return contador
+}
+
+export default contarRango
