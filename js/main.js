d108e6e701d3eee988c69d97fac56a8c1c6fe6bb
OugMontiel <88790286+OugMontiel@users.noreply.github.com>
2024-04-19 16:17:21 -0500

    feat: :ambulance: Conexion de a wed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 import prompt from 'async-prompt'
+import { getAllProfile, postProfile } from './modulos/camper.js';
+
+
+// console.log(await getAllProfile());
+
+let name = await prompt("Ingrese el nombre: ")
+console.log(await postProfile(name));
--- js/modulos/camper.js
+++ js/modulos/camper.js
@@ -1,1 +1,16 @@
+const getAllProfile = async()=>{
+    let conexion = await fetch("http://localhost:3000/profile",{method:"GET"});
+    let data= await conexion.json();
+    return data;
+}
+const postProfile = async()=>{
+    let conexion = await fetch("http://localhost:3000/profile",{
+        method:"POST",
+        body:JSON.stringify({name})
+    });
+    let data= await conexion.json();
+    return data;
 
+}
+
+export default [getAllProfile, postProfile];
