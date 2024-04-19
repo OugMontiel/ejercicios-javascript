d15f2838208c2c42d7f0163f879bcd64664be2e8
OugMontiel <88790286+OugMontiel@users.noreply.github.com>
2024-04-19 15:03:30 -0500

    feat: :sparkles: Ejersicio Finalizado

---
+++ js/main.js
@@ -0,0 +1,4 @@
+import prompt from "async-prompt";
+import numeroDeAes from "./modulos/ej9.js";
+
+console.log(numeroDeAes("abracadabra"))
---
+++ js/modulos/ej9.js
@@ -0,0 +1,10 @@
+let numeroDeAes = (cadena,letra="a",bandera=true)=>{
+    let cont=0;
+    cadena.split("").forEach(char=>{
+        if(char == letra && bandera) cont++;
+        else if(char == letra.toUpperCase()) cont++;
+    });
+    return cont;
+}
+
+export default numeroDeAes
