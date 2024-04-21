a11d249ff938ae7a8c6631615031eff3d4d0bd58
Diego Alejandro Montiel Florez <88790286+OugMontiel@users.noreply.github.com>
2024-04-21 23:14:50 +0000

    feat: :sparkles: ejercicio 11
    Usamos de nuevo .length y funiona tanto en caracteras como en arreglos y por ultimo usamos el += para poder sumar y acomular las respestivo resultado

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
 import prompt from 'async-prompt'
-// import *** from './modulos/ej.js'
+import sumarArreglo from './modulos/ej11.js'
+
+// código de prueba
+console.log(sumarArreglo([3, 1, 2])) // 6
+console.log(sumarArreglo([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])) // 55
+console.log(sumarArreglo([])) // 0
---
+++ js/modulos/ej11.js
@@ -0,0 +1,10 @@
+let sumarArreglo = (arr)=>{
+    let resultado =0
+    for (let i=0;i<arr.length;i++){
+        resultado += arr[i]
+    }
+
+    return resultado
+}
+
+export default sumarArreglo
