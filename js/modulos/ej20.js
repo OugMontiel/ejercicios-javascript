b550160b96c28595f061a36c798eb81a7f35be17
Diego Alejandro Montiel Florez <88790286+OugMontiel@users.noreply.github.com>
2024-04-22 13:41:30 +0000

    feat: ✨ ejercicio 20
    Usamos in if .... para poder omiti los espacios

--- js/modulos/ej20.js
+++ js/modulos/ej20.js
@@ -1,15 +1,16 @@
 let password = (cadena)=>{
     let resultado =""
     for (let i=0;i<cadena.length;i++){
-        resultado+=(
-            (cadena[i]===cadena[i].toUpperCase())?cadena[i].toLowerCase():
-            (cadena[i]===" ")?"":
-            (cadena[i]==="a")?"4":
-            (cadena[i]==="e")?"3":
-            (cadena[i]==="i")?"1":
-            (cadena[i]==="o")?"0":
-            cadena[i]
-        );
+        if (cadena[i]!==" "){
+            resultado+=(
+                (cadena[i]===cadena[i].toUpperCase())?cadena[i].toLowerCase():
+                (cadena[i]==="a")?"4":
+                (cadena[i]==="e")?"3":
+                (cadena[i]==="i")?"1":
+                (cadena[i]==="o")?"0":
+                cadena[i]
+            );
+        }
     }
 
     return resultado
