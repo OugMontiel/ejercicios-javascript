ddad33c17ae676768a181c517737d1ec135ebef5
Diego Alejandro Montiel Florez <88790286+OugMontiel@users.noreply.github.com>
2024-04-22 16:02:41 +0000

    feat: :sparkles: ejercicio 26
    For anidado

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 import prompt from 'async-prompt'
-// import *** from './modulos/ej.js'
+import imprimirMatriz from './modulos/ej26.js'
+
+// código de prueba
+console.log(imprimirMatriz([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ]))
+  
+  // 1
+  // 2
+  // 3
+  // 4
+  // 5
+  // 6
+  // 7
+  // 8
+  // 9
---
+++ js/modulos/ej26.js
@@ -0,0 +1,9 @@
+let imprimirMatriz = (matrix)=>{
+    for (let i of matrix){
+        for(let j of i){
+            console.log(j)
+        }
+    }
+}
+
+export default imprimirMatriz
