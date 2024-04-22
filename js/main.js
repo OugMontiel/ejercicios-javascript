b3899f4ad811700c8ce72ebaac0315d7e865d569
Diego Alejandro Montiel Florez <88790286+OugMontiel@users.noreply.github.com>
2024-04-22 01:00:18 +0000

    feat: :construction: un erro al momento de omitir el vacio

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
 import prompt from 'async-prompt'
-// import *** from './modulos/ej.js'
+import password from './modulos/ej20.js'
+
+// código de prueba
+console.log(password("hola")) // "h0l4"
+console.log(password("esta es una prueba")) // "3st43sun4pru3b4"
+console.log(password("")) // ""
